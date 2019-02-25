9c3900ab69cbfff014d607b041d15c113943a957
MAkcster <47952310+MAkcster@users.noreply.github.com>
2019-02-25 11:23:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const cfg require('./index.json');
 const prefix = (cfg.prefix);
+client.login('NTQ5MzE5NzE2OTQ3MDk5NjQ4.D1VSVQ.Xp_BtqpHKMsyQxrqj5kdqF7Ap6s');
+client.on('message',message{
+    if(message.content === "tu fais quoi?"){
+    message.reply('rien');
+    console.log('répond a tfq');
+}
+})
 const token = process.env.token;
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
